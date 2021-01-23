9fa0da20805d7992f47e6f9b1d77b09dd7be8788
ndraa124 <indradavidpesik@gmail.com>
2021-01-23 22:22:40 +0800

    bugfix feature cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { createCart, getAllCartByCsId, deleteCartByCsId, updateCartByCsId } = require('../src/controllers/CartController')
+const { createCart, getAllCartByCsId, deleteCartByCrId, updateCartByCrId } = require('../src/controllers/CartController')
 
 const uploadImage = require('../src/middleware/multer')
 const { authorization } = require('../src/middleware/auth')
 
 router.post('/', authorization, uploadImage, createCart)
 router.get('/:csId', authorization, getAllCartByCsId)
-router.delete('/:csId', authorization, deleteCartByCsId)
-router.put('/:csId', authorization, uploadImage, updateCartByCsId)
+router.delete('/:crId', authorization, deleteCartByCrId)
+router.put('/:crId', authorization, uploadImage, updateCartByCrId)
 
 module.exports = router
