58aa13b0188a166a051c1faad624873345973c5c
allukman <allukman1802@gmail.com>
2021-01-28 10:32:05 +0700

    detail account customer

--- routes/customer.js
+++ routes/customer.js
@@ -4,8 +4,9 @@ const router = express.Router()
 const { updateCustomerByCsId, getCustomerByCsId } = require('../src/controllers/CustomerController')
 
 const uploadImage = require('../src/middleware/multer')
+const { authorization } = require('../src/middleware/auth')
 
-router.put('/:csId', uploadImage, updateCustomerByCsId)
-router.get('/:csId', getCustomerByCsId)
+router.put('/:csId', authorization, uploadImage, updateCustomerByCsId)
+router.get('/:csId', authorization, getCustomerByCsId)
 
 module.exports = router
