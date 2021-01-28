737d6fa3d2694c077ccff4a1efa6bab22ef35868
ndraa124 <indradavidpesik@gmail.com>
2021-01-28 09:05:30 +0800

    update feature orders

--- src/models/OrdersModel.js
+++ src/models/OrdersModel.js
@@ -1,5 +1,9 @@
 const dbConnect = require('../../config/db.config')
 
+const {
+  getAllCartByCsId
+} = require('../models/CartModel')
+
 module.exports = {
   createOrders: (data) => {
     return new Promise((resolve, reject) => {
@@ -8,8 +12,26 @@ module.exports = {
                 SET ?
       `
 
-      dbConnect.query(query, data, (error, results, _fields) => {
+      dbConnect.query(query, data, async (error, results, _fields) => {
         if (!error) {
+          const dataCart = []
+          const cart = await getAllCartByCsId(data.cs_id)
+
+          for (let i = 0; i < cart.length; i++) {
+            const item = cart[i]
+
+            dataCart[i] = {
+              cs_id: item.cs_id,
+              or_id: results.insertId,
+              ht_product: item.cr_product,
+              ht_price: item.cr_price,
+              ht_qty: item.cr_qty,
+              ht_total: item.cr_total
+            }
+          }
+
+          console.log(dataCart)
+
           resolve(results)
         } else {
           reject(error)
