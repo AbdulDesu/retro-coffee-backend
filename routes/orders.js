e43ab7ec908515df6992202f8dfd930d4bf27f70
ndraa124 <indradavidpesik@gmail.com>
2021-02-08 09:04:49 +0800

    update order

--- routes/orders.js
+++ routes/orders.js
@@ -15,6 +15,6 @@ router.post('/', authorization, createOrders)
 router.put('/', authorization, updateOrdersStatus)
 router.get('/:csId', authorization, getAllTransaction)
 router.get('/', authorization, getAllTransactionById)
-router.get('/all/', authorization, getAllOrder)
+router.get('/all', authorization, getAllOrder)
 
 module.exports = router
